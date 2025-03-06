1f04e24f32204fb322750e748f206ed2bd81a5bf
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-03-06 01:18:57 -0300

    feat: Define entidade RunnerModel e implementa os atributos. Define a entidade TrainingModel e define atributos

--- src/main/java/dev/vinitech/CadastroDeCorredores/runner/model/RunnerModel.java
+++ src/main/java/dev/vinitech/CadastroDeCorredores/runner/model/RunnerModel.java
@@ -1,4 +1,38 @@
 package dev.vinitech.CadastroDeCorredores.runner.model;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+@Entity
+@Table(name = "tb_runners")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RunnerModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String gender; // TODO: Fazer um Enum genero
+
+    private double weight; // Em quilogramas(kg)
+    private double height; // Em centimetros(cm)
+
+    private String category; // TODO: Fazer um Enum de categoria (amador, profissional, casual etc)
+    private String team; // Equipe de corrida ou Individual
+
+    @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<TrainingModel> trainings;
+
 }
---
+++ src/main/java/dev/vinitech/CadastroDeCorredores/training/model/TrainingModel.java
@@ -0,0 +1,44 @@
+package dev.vinitech.CadastroDeCorredores.training.model;
+
+import dev.vinitech.CadastroDeCorredores.runner.model.RunnerModel;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_trainings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TrainingModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TrainingType type; // TODO: Enum com tipos de treino(Longão, Ritmo, Intervalado, Aclive...)
+
+    @Column(nullable = false)
+    private double distance; // em KM
+
+    @Column(nullable = false)
+    private Duration duration;
+
+    private double pace; // ritmo médio (min/km), TODO: criar formula depois
+
+    @ManyToOne
+    @JoinColumn(name = "runner_id", nullable = false) // Aqui cria a chave estrangeira
+    private RunnerModel runner;
+
+    @Enumerated(EnumType.STRING)
+    private TrainingZone zone; // TODO: Criar classe zona de treinamento
+
+}
