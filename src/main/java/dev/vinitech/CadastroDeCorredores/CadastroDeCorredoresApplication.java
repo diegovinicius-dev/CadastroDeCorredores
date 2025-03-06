ca47f5d1d389a5daa97806984c5ab2c68d203255
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-03-05 22:14:03 -0300

    first commit

---
+++ src/main/java/dev/vinitech/CadastroDeCorredores/CadastroDeCorredoresApplication.java
@@ -0,0 +1,13 @@
+package dev.vinitech.CadastroDeCorredores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCorredoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCorredoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vinitech/CadastroDeCorredores/CadastroDeCorredoresApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinitech.CadastroDeCorredores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCorredoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
