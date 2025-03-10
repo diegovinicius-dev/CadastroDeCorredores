7656d822ecde6d4696e0e203b9b4f31e401d5f88
Diego Vinicius <diegocavalcante.tech@gmail.com>
2025-03-10 15:23:59 -0300

    feat: Atualiza o Controller com mock de dados

--- src/main/java/dev/vinitech/CadastroDeCorredores/runner/controller/RunnerController.java
+++ src/main/java/dev/vinitech/CadastroDeCorredores/runner/controller/RunnerController.java
@@ -1,8 +1,6 @@
 package dev.vinitech.CadastroDeCorredores.runner.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/runners")
@@ -11,6 +9,36 @@ public class RunnerController {
     @GetMapping("/teste")
     public String testApi() {
         return "Runner API funcionando!";
+    }
+
+    // Adicionar Corredor (CREATE)
+    @PostMapping("/criar")
+    public String criarCorredor() {
+        return "Corredor criado";
+    }
+
+    // Mostrar todos os corredores (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsCorredores() {
+        return "Mostrar corredores";
+    }
+
+    // Mostrar corredores por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsCorredoresPorId() {
+        return "Mostrar corredores por ID";
+    }
+
+    // Alterar dados dos corredores (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarCorredorPorId() {
+        return "Alterar corredor por ID";
+    }
+
+    // Deletar Corredor (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarCorredorPorId() {
+        return "Corredor deletado por ID";
     }
 
 }
